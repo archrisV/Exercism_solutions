9c7f0b1726d18b3e3746bebcf206c350ecb252b6
Christian Torbica <uplink.chris@gmail.com>
2020-02-19 17:49:47 +0100

    javascript/matching-brackets.js

--- javascript/matching-brackets/matching-brackets.js
+++ javascript/matching-brackets/matching-brackets.js
@@ -1,3 +1,22 @@
-export const matchingBrackets = () => {
-  throw new Error("Remove this statement and implement this function");
+export const matchingBrackets = (text) => {
+  let stack = [];
+  let keys = [...bracketPairs.keys()].join("");
+  let vals = [...bracketPairs.values()].join("");
+  for(let symbol of text){
+    if (vals.includes(symbol)){
+      stack.push(symbol);
+    } else if (keys.includes(symbol)){
+      let top = stack.pop();
+      if(top === undefined || bracketPairs.get(symbol) !== top){
+        return false;
+      }
+    }
+  }
+  return stack.length == 0;
 };
+
+const bracketPairs = new Map([
+  ["]", "["],
+  ["}", "{"],
+  [")", "("]
+]);
