54784b0187f94517b8d3519dd2099ba21a036778
Christian Torbica <uplink.chris@gmail.com>
2020-02-24 23:21:35 +0100

    javascript/nucleotide-count

--- javascript/nucleotide-count/nucleotide-count.js
+++ javascript/nucleotide-count/nucleotide-count.js
@@ -1,5 +1,10 @@
 export class NucleotideCounts {
-  static parse() {
-    throw new Error("Remove this statement and implement this function");
+  static parse(dnaStrand) {
+    if([...dnaStrand].filter(x => !"ACGT".includes(x)).length !== 0){
+      throw new Error('Invalid nucleotide in strand');
+    }
+    return `${nucleotideCount(dnaStrand, 'A')} ${nucleotideCount(dnaStrand, 'C')} ${nucleotideCount(dnaStrand, 'G')} ${nucleotideCount(dnaStrand, 'T')}`;
   }
 }
+
+const nucleotideCount = (strand, nucleotide) => [...strand].filter(x => nucleotide === x).length;
