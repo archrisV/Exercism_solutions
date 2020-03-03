325136aca4d19dde3ffde6cb496374161a447ab3
Christian Torbica <uplink.chris@gmail.com>
2020-03-03 20:39:13 +0100

    java/hamming

--- java/hamming/src/main/java/Hamming.java
+++ java/hamming/src/main/java/Hamming.java
@@ -1,27 +1,24 @@
 class Hamming {
-    private String left;
-    private String right;
+    private final int hammingDistance;
 
     Hamming(String leftStrand, String rightStrand) {
-        if(leftStrand == null || (leftStrand == "" && rightStrand.length() != 0)){
+        if(leftStrand == null || (leftStrand.isEmpty() && !rightStrand.isEmpty())){
             throw new IllegalArgumentException("left strand must not be empty.");
         }
-        if(rightStrand == null || (rightStrand == "" && leftStrand.length() != 0)){
+        if(rightStrand == null || (rightStrand.isEmpty() && !leftStrand.isEmpty())){
             throw new IllegalArgumentException("right strand must not be empty.");
         }
         if(leftStrand.length() != rightStrand.length()){
             throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
         }
-        left = leftStrand;
-        right = rightStrand;
+        int distance = 0;
+        for(int i = 0; i < leftStrand.length(); i++ ){
+            if(leftStrand.charAt(i) != rightStrand.charAt(i)) distance++;
+        }
+        hammingDistance = distance;
     }
 
     int getHammingDistance() {
-        int distance = 0;
-        for(int i = 0; i < left.length(); i++ ){
-            if(left.charAt(i) != right.charAt(i)) distance++;
-        }
-        return distance;
+        return hammingDistance;
     }
-
 }
