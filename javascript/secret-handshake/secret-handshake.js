ef1760fac53820576b32d09256c53a8561fbe31e
Christian Torbica <uplink.chris@gmail.com>
2020-03-05 15:27:26 +0100

    javascript/secret-handshake

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -4,11 +4,6 @@ export const secretHandshake = (number) => {
   if(!Number.isInteger(number)){
     throw new Error('Handshake must be a number');
   } 
-  let result = [];
-  for(let i = 0; i < answers.length; i++){
-    if((number & Math.pow(2, i))){
-      result.push(answers[i]);
-    }
-  }
+  let result = answers.filter((x, i) => number & Math.pow(2, i));
   return (number & 16) ? result.reverse() : result;
 };
