f6b321bd75bdd7fb547d8bb875f4eeb274000ec6
Christian Torbica <uplink.chris@gmail.com>
2020-03-05 20:18:24 +0100

    javascript/secret-handshake

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -1,9 +1,10 @@
-const answers = ['wink', 'double blink', 'close your eyes', 'jump']
+const answers = ['wink', 'double blink', 'close your eyes', 'jump'];
+const reversalMask = 16;
 
 export const secretHandshake = (number) => {
   if(!Number.isInteger(number)){
     throw new Error('Handshake must be a number');
   } 
   let result = answers.filter((x, i) => number & Math.pow(2, i));
-  return (number & 16) ? result.reverse() : result;
+  return (number & reversalMask ) ? result.reverse() : result;
 };
