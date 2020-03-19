c2c48dac558121b2ecf331355f939ab1ea62cd27
Christian Torbica <uplink.chris@gmail.com>
2020-03-19 13:44:00 +0100

    csharp/diffie-hellman

---
+++ csharp/diffie-hellman/DiffieHellman.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Numerics;
+
+public static class DiffieHellman
+{
+    public static BigInteger PrivateKey(BigInteger primeP) => new BigInteger(new Random().Next(1, (int) primeP - 1));
+    public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) => BigInteger.ModPow(primeG, privateKey, primeP);
+    public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) => BigInteger.ModPow(publicKey, privateKey, primeP);
+}
